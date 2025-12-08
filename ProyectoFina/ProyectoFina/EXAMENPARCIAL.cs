9826e6568a49f5540562b9fd00b9080080604b80
itzmefabry <rasf080924hgtnrba0@cbtis217.edu.mx>
2025-12-08 14:51:50 -0600

    PLANTILLA PORQUE NOS CAMBIARON TODO

---
+++ ProyectoFina/ProyectoFina/EXAMENPARCIAL.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Fabry UwU
+ * Date: 08/12/2025
+ * Time: 02:36 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProyectoFina
+{
+	/// <summary>
+	/// Description of EXAMENPARCIAL.
+	/// </summary>
+	public partial class EXAMENPARCIAL : Form
+	{
+		public EXAMENPARCIAL()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
